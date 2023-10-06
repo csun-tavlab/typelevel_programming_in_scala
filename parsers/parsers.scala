7926f41c2ac81f8c61a26698d9b4eb74027efdc1
Kyle Dewey <kyle.dewey@csun.edu>
2023-10-06 12:09:19 -0700

    the parser itself needs user specification

--- parsers/parsers.scala
+++ parsers/parsers.scala
@@ -2,12 +2,13 @@ sealed trait HList
 case object HNil extends HList
 case class HCons[Head, Tail <: HList](head: Head, tail: Tail) extends HList
 
-// what you really want to say is at compile time, you know
-// what Parsed will be for some input token prefix
+// And[Elem[A.type], Elem[B.type]]
+sealed trait PreParser
+case class Elem[A](a: A) extends PreParser
+case class And[P1 <: PreParser, P2 <: PreParser](p1: P1, p2: P2) extends PreParser
+case class Or[P1 <: PreParser, P2 <: PreParser](p1: P1, p2: P2) extends PreParser
 
-// trait Parser[HCons[Token1, HCons[Token2, Rest]],
-//              Rest, A]
-trait Parser[InputTokens <: HList] {
+trait Parser[PP <: PreParser, InputTokens <: HList] {
   type OutputTokens <: HList
   type Parsed
   def parse(input: InputTokens): (OutputTokens, Parsed)
@@ -15,83 +16,66 @@ trait Parser[InputTokens <: HList] {
 
 object Parser {
   type ParserAux[
+    PP <: PreParser,
     InputTokens <: HList,
     Out <: HList,
     P] =
-    Parser[InputTokens] {
+    Parser[PP, InputTokens] {
       type OutputTokens = Out
       type Parsed = P
     }
 
   implicit def elem[
-    Elem,
-    Rest <: HList]: ParserAux[HCons[Elem, Rest], Rest, Elem] = {
-    new Parser[HCons[Elem, Rest]] {
+    A,
+    Rest <: HList]: ParserAux[Elem[A], HCons[A, Rest], Rest, A] = {
+    new Parser[Elem[A], HCons[A, Rest]] {
       type OutputTokens = Rest
-      type Parsed = P
-      def parse(input: HCons[Elem, Rest]): (Rest, Elem) = {
+      type Parsed = A
+      def parse(input: HCons[A, Rest]): (Rest, A) = {
         (input.tail, input.head)
       }
     }
   }
 
-  def elem[
-    Elem,
-    P,
-    Rest <: HList](f: Elem => P): ParserAux[HCons[Elem, Rest], Rest, P] = {
-    new Parser[HCons[Elem, Rest]] {
-      type OutputTokens = Rest
-      type Parsed = P
-      def parse(input: HCons[Elem, Rest]): (Rest, P) = {
-        (input.tail, f(input.head))
-      }
-    }
-  }
-
-  def and[
+  implicit def and[
+    P1 <: PreParser,
+    P2 <: PreParser,
     List1 <: HList,
     List2 <: HList,
     List3 <: HList,
     A,
     B
-  ](p1: ParserAux[List1, List2, A], p2: ParserAux[List2, List3, B]): ParserAux[List1, List3, (A, B)] = {
-    new Parser[List1] {
+  ](implicit ev1: ParserAux[P1, List1, List2, A],
+    ev2: ParserAux[P2, List2, List3, B]): ParserAux[And[P1, P2], List1, List3, (A, B)] = {
+    new Parser[And[P1, P2], List1] {
       type OutputTokens = List3
       type Parsed = (A, B)
-      def parse(input: List1): (List3, (A, B)) = {
-        val (list2, a) = p1.parse(input)
-        val (list3, b) = p2.parse(list2)
+      def parse(list1: List1): (List3, (A, B)) = {
+        val (list2, a) = ev1.parse(list1)
+        val (list3, b) = ev2.parse(list2)
         (list3, (a, b))
       }
     }
   }
 
-  def map[
-    List1 <: HList,
-    List2 <: HList,
-    A,
-    B](p: ParserAux[List1, List2, A], f: A => B): ParserAux[List1, List2, B] = {
-    new Parser[List1] {
-      type OutputTokens = List2
-      type Parsed = B
-      def parse(input: List1): (List2, B) = {
-        val (list2, a) = p.parse(input)
-        (list2, f(a))
-      }
-    }
+  def parse[
+    PP <: PreParser,
+    InputTokens <: HList,
+    Result
+  ](pp: PP, input: InputTokens)(implicit ev: ParserAux[PP, InputTokens, HNil.type, Result]): Result = {
+    ev.parse(input)._2
   }
 }
 
-// exp ::= a b c
 object A {}
 object B {}
 object C {}
 
 object Test {
   import Parser._
-  val test: Int = and(elem((a: A.type) => a), elem((b: B.type) => b))
-
-  // val test = and(elem((a: A.type) => a),
-  //   and(elem((b: B.type) => b),
-  //     elem((c: C.type) => c)))
+  def main(args: Array[String]) {
+    println(parse(Elem(A), HCons(A, HNil)))
+    println(parse(And(Elem(A), Elem(B)), HCons(A, HCons(B, HNil))))
+    
+  }
 }
